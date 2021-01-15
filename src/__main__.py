7b1ea5f1d642d70cfdf9403b72e91ae69931e139
Ryan Peters <petersryan84@gmail.com>
2021-01-15 12:33:16 -0800

    updated src/__main__.py to better expose doit tasks for future terminal entry points

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,7 @@
 import subprocess
 import os
-# import pathlib as pl
 from pathlib import Path
-import argparse
 
-
-def console_arg_parse():
-    parser = argparse.ArgumentParser("email_classifier")
-    # ToDo: Define console arguments
-    return parser.parse_args()
 
 def main():
     here = Path(__file__).resolve().parent
@@ -19,6 +12,6 @@ def main():
     finally:
         os.chdir(old_cwd)
 
-# if __name__ == '__main__':
-#     exec_d = task_init_configs()
-#     func = exec_d.pop("actions")[0](**exec_d)
+
+if __name__ == '__main__':
+    main()
